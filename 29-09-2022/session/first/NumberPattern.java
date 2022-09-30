ec9220e12ef5d641811352116ffd4de62de5b7cf
TejasKute151200 <113657741+TejasKute151200@users.noreply.github.com>
2022-09-30 18:54:10 +0530

    Add files via upload

---
+++ 29-09-2022/session/first/NumberPattern.java
@@ -0,0 +1,49 @@
+package session.first;
+
+						/* Print Number Pattern */ 
+//1
+//12
+//123
+//1234
+//12345
+//123456
+//1234567
+
+//public class NumberPattern {
+//
+//	public static void main(String[] args) {
+//		
+//		for(int i=1;i<=7;i++)
+//		{
+//			 for(int j = 1;j<=i;j++)
+//			 {
+//				 System.out.print(j);
+//			 }
+//			 System.out.println();
+//		}
+//		
+//	}
+//}
+
+
+//1
+//10
+//101
+//1010
+//10101
+//101010
+
+public class NumberPattern {
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=1;i++)
+		{
+			 for(int j = 1;j<=i;j--)
+			 {
+				 System.out.print(j);
+			 }
+			 System.out.println();
+		}
+	}
+}
+
