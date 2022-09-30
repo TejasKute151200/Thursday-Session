577ae244b76aa15e6b43da975cb7032d0230ed31
TejasKute151200 <113657741+TejasKute151200@users.noreply.github.com>
2022-09-30 17:43:48 +0530

    Add files via upload

---
+++ 29-09-2022/session/first/StarPattern.java
@@ -0,0 +1,42 @@
+package session.first;
+
+						/* Print Right Angled Triangle Pattern */
+// 	*
+// 	**
+// 	***
+// 	****
+// 	*****
+//
+//public class StarPattern {
+//	public static void main(String[] args) {
+//		for(int r= 0; r<=4; r++) {
+//			for(int c=0; c<=r; c++) {
+//				System.out.print("*");
+//			}
+//			System.out.println(" ");
+//		}
+//	}
+//
+//}
+
+
+									/* Print Triangle Pattern */
+//		*
+//	   * *
+//	  * * *
+//	 * * * *
+//	* * * * *
+//
+//public class StarPattern{
+//	public static void main(String[] args) {
+//		for(int i=0;i<5;i++) {
+//			for(int j=i;j<(5-1);j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k=0;k<=i;k++) {
+//				System.out.print(" *");
+//			}
+//			System.out.println("\n");
+//		}
+//	}
+//}
