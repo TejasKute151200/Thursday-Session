c9b104624af19c2a9f70aaf7fd39de9567b186b2
TejasKute151200 <113657741+TejasKute151200@users.noreply.github.com>
2022-10-07 19:01:52 +0530

    Add files via upload

--- 29-09-2022/session/first/StarPattern.java
+++ 29-09-2022/session/first/StarPattern.java
@@ -75,34 +75,10 @@ package session.first;
 //**** ****
 //***** *****
 
-public class StarPattern{
-	public static void main(String[] args) {
-		int i, j, k,l;
-		
-		for(i=1;i<6;i++) {
-			for(j=0;j<(i*2)+1;j++) {
-			if(( j<5 || j>1 )) {
-				System.out.print(" ");
-			}
-			else {
-				System.out.print("*");
-			}
-			}
-			System.out.println();
-		}
-		
-		
-//			for (i = 1; i < 6; i++) {
-//				for (j = 0; j < ((i * 2) + 1); j++) {
-//					if ( j == i ){
-//					System.out.print(" ");
-//					}
-//					else {	
-//						System.out.print("*");
-//					}
-//				}
-//				System.out.println();
-//			}
+//public class StarPattern{
+//	public static void main(String[] args) {
+//		int i, j, k,l;		
+//		
 //		for (i= 5; i > 0; i-- ) {
 //			for (j=((i*2)+1);j>0; j--) {
 //				if (j==i+1) {
@@ -125,8 +101,44 @@ public class StarPattern{
 //			}
 //		}
 //		System.out.println();
+//		}	
+//	}
+//}
+
+
+//****
+//*****
+//******
+//*******
+//********
+//*********
+//**********
+//**********
+//*********
+//********
+//*******
+//******
+//*****
+//****
+//***
+//**
+//*
+
+//public class StarPattern {
+//	public static void main(String[] args) {
+//		int i, j, k, l;
+//
+//		for (i = 4; i<11;i++) {
+//			for(k=0;k<i;k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
 //		}
-		
-		
-	}
-}
+//		for (j= 10; j> 0; j-- ) {
+//			for (l=j;l>0; l--) {
+//					System.out.print("*");	
+//			}
+//			System.out.println();
+//		}	
+//	}
+//}
